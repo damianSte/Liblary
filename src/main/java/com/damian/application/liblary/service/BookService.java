b91209e4b87db2ce9fef3a726c12fdd2340070ac
Damian Stępień <151776252+damianSte@users.noreply.github.com>
2024-03-12 20:58:07 +0100

    update

--- src/main/java/com/damian/application/liblary/service/BookService.java
+++ src/main/java/com/damian/application/liblary/service/BookService.java
@@ -1,2 +1,25 @@
-package com.damian.application.liblary.service;public class BookService {
+package com.damian.application.liblary.service;
+
+import com.damian.application.liblary.infrastucture.entity.BookEntity;
+import com.damian.application.liblary.infrastucture.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public List<BookEntity> getAll(){
+        return bookRepository.findAll();
+    }
+
+   public BookEntity getOne (long book_id){
+        return bookRepository.findById(book_id).orElseThrow(()->new RuntimeException("Book not found"));
+   }
+
+    public BookEntity create (BookEntity book){
+        return bookRepository.save(book);
+    }
 }
